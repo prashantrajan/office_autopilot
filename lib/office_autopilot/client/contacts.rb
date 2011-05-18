388002052cb882a64ff6caf932865ebe306ef782
Prashant Nadarajan <pair+prn@pivotallabs.com>
2011-05-18 14:15:38 +0800

    added #contacts_fetch_sequences, #contacts_key, #contacts_fetch

--- lib/office_autopilot/client/contacts.rb
+++ lib/office_autopilot/client/contacts.rb
@@ -16,9 +16,25 @@ module OfficeAutopilot
         parse_contacts_xml(response)[0]
       end
 
-      def contacts_pull_tags
+      def contacts_pull_tag
         response = request(:post, CONTACTS_ENDPOINT, :body => {'reqType' => 'pull_tag'})
-        parse_pull_tags_xml(response)
+        parse_xml(response, "tag")
+      end
+
+      def contacts_fetch_sequences
+        response = request(:post, CONTACTS_ENDPOINT, :body => {'reqType' => 'fetch_sequences'})
+        parse_xml(response, "sequence")
+      end
+
+      def contacts_key
+        response = request(:post, CONTACTS_ENDPOINT, :body => {'reqType' => 'key'})
+        parse_contacts_key_xml(response)
+      end
+
+      def contacts_fetch(ids)
+        xml = xml_for_fetch("contact", ids)
+        response = request(:post, CONTACTS_ENDPOINT, :body => {'reqType' => 'fetch', 'data' => xml})
+        parse_contacts_xml(response)
       end
 
       private
@@ -44,9 +60,52 @@ module OfficeAutopilot
         contacts
       end
 
+      def parse_contacts_key_xml(response)
+        groups = {}
+
+        xml = Nokogiri::XML(response)
+        xml.css('result contact Group_Tag').each do |group_tag|
+          group = { 'fields' => {} }
+          group['editable'] = group_tag['editable'] == '1'
+
+          group_tag.css('field').each do |field_node|
+            field_type = field_node['type']
+            field_info = { 'type' => field_type }
+            field_info['editable'] = field_node['editable'] == '1'
+
+            case field_type
+              when 'tdrop'
+                options = []
+                field_info['options'] = options
+                field_node.css('option').each do |option_node|
+                  options << option_node.content
+                end
+              when 'list'
+                list = {}
+                field_info['list'] = list
+                field_node.css('list').each do |list_node|
+                  list[list_node['id']] = list_node.content
+                end
+            end
+            group['fields'][field_node['name']] = field_info
+          end
+          groups[group_tag['name']] = group
+        end
+        groups
+      end
+
+      def parse_xml(response, element_name)
+        result = {}
+        xml = Nokogiri::XML(response)
+        xml.css("result #{element_name}").each do |node|
+          id = node['id']
+          result[id] = node.content
+        end
+        result
+      end
+
       def xml_for_contact(options)
         attrs = {}
-
         id = options.delete('id')
         attrs[:id] = id if id
 
@@ -60,6 +119,14 @@ module OfficeAutopilot
             end
           end
         end
+      end
+
+      def xml_for_fetch(type, ids)
+        xml = ""
+        ids.each do |id|
+          xml << "<#{type}_id>#{id}</#{type}_id>"
+        end
+        xml
       end
 
       def xml_for_search(options)
@@ -77,16 +144,6 @@ module OfficeAutopilot
             end
           end
         end
-      end
-
-      def parse_pull_tags_xml(response)
-        result = {}
-        xml = Nokogiri::XML(response)
-        xml.css("result tag").each do |node|
-          id = node['id']
-          result[id] = node.content
-        end
-        result
       end
 
     end
